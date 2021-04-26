7e1c902bac12f499c133d352a94701cce0d4af76
Surbhit Vishwakarma <83189259+surbhit-7@users.noreply.github.com>
2021-04-26 20:29:28 +0530

    Update MinimumCostRopes.cpp

--- MinimumCostRopes.cpp
+++ MinimumCostRopes.cpp
@@ -1,4 +1,4 @@
-// Difficulty - Medium
+// Difficulty - Easy
 /* Problem Statement :- 
 There are given N ropes of different lengths, we need to connect these ropes into one rope. The cost to connect two ropes is equal to sum of their lengths. The task is to connect the ropes with minimum cost.
 
